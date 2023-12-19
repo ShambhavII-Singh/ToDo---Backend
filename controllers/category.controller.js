8eaa30aae52d0b57a9c42ce63e8e4d45d5633586
ShambhavII-Singh <shambhavi.singh2021@vitstudent.ac.in>
2023-12-19 23:35:12 +0530

    create category controller

---
+++ controllers/category.controller.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+import Category from '../mongodb/models/category.js';
+
+const getCategories = async (req, res) => {
+    
+};
+
+const createCategory = async (req, res) => {
+    
+};
+
+const updateCategory = async (req, res) => {
+    
+};
+
+const deleteCategory = async (req, res) => {
+    
+};
+
+export {
+    getCategories,
+    createCategory,
+    updateCategory,
+    deleteCategory,
+};

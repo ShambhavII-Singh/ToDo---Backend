c1788e35a8e315ed70e75b2fe1be613995d1ffa4
ShambhavII-Singh <shambhavi.singh2021@vitstudent.ac.in>
2023-12-19 23:35:36 +0530

    create task controller

---
+++ controllers/task.controller.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+import Task from '../mongodb/models/task.js';
+import Category from '../mongodb/models/category.js';
+
+const getTasks = async (req, res) => {
+    
+};
+
+const createTask = async (req, res) => {
+    
+};
+
+const completeTask = async (req, res) => {
+    
+};
+
+const priorityTask = async (req, res) => {
+    
+};
+
+const changeCategory = async (req, res) => {
+    
+};
+
+const deleteTask = async (req, res) => {
+    
+};
+
+export {
+    getTasks,
+    createTask,
+    completeTask,
+    priorityTask,
+    changeCategory,
+    deleteTask,
+};

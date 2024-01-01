ea4b28f553faa48bca3396762588e39387dc067f
ShambhavII-Singh <shambhavi.singh2021@vitstudent.ac.in>
2024-01-01 16:39:31 +0530

    user model

---
+++ controllers/user.controller.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+import User from '../mongodb/models/user.js';
+
+const getUser = async (req, res) => {
+    
+};
+
+const createUser = async (req, res) => {
+    
+};
+
+const updateUser = async (req, res) => {
+    
+};
+
+const deleteUser = async (req, res) => {
+    
+};
+
+export {
+    getUser,
+    createUser,
+    updateUser,
+    deleteUser,
+};

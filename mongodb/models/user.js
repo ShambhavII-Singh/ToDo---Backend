6664afbe276d4a06146fe368c0ff1e790e69c356
ShambhavII-Singh <shambhavi.singh2021@vitstudent.ac.in>
2024-01-01 16:40:15 +0530

    user model

---
+++ mongodb/models/user.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const userSchema = mongoose.Schema({
+    email: {
+        type: String, 
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+        required: true
+    },
+    allCategories: [{
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "Category" 
+    }],
+    allTasks: [{
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "Task" 
+    }]
+})
+
+const userModel = mongoose.model("User",userSchema);
+
+export default userModel;

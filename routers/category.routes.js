a9ccc60284b7e3aec85de6a5a9b7011eca4e0854
ShambhavII-Singh <shambhavi.singh2021@student.ac.in>
2024-06-28 20:20:42 +0530

    category routes

--- routers/category.routes.js
+++ routers/category.routes.js
@@ -1,12 +1,11 @@
 import express from 'express';
 
-import { getCategories,createCategory,updateCategory,deleteCategory } from '../controllers/category.controller';
+import { getCategories,createCategory,updateCategory } from '../controllers/category.controller.js';
 
 const router = express.Router();
 
 router.route('/').get(getCategories);
 router.route('/').post(createCategory);
 router.route('/:id').patch(updateCategory);
-router.route('/:id').delete(deleteCategory);
 
 export default router;

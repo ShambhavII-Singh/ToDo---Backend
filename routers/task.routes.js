051339b06106216055f1e1fc39ae231193c4852e
ShambhavII-Singh <shambhavi.singh2021@student.ac.in>
2024-06-28 20:21:00 +0530

    task routes

--- routers/task.routes.js
+++ routers/task.routes.js
@@ -1,14 +1,17 @@
 import express from 'express';
 
-import { getTasks,createTask,completeTask,priorityTask,changeCategory,deleteTask } from '../controllers/task.controller';
+import { getTasks,createTask,completeTask,priorityTask,updateTask,deleteTask } from '../controllers/task.controller.js';
 
 const router = express.Router();
 
 router.route('/').get(getTasks);
+
 router.route('/').post(createTask);
-router.route('/:id').patch(completeTask);
-router.route('/:id').patch(priorityTask);
-router.route('/:id').patch(changeCategory);
+
+router.route('/:id/status').patch(completeTask);
+router.route('/:id/priority').patch(priorityTask);
+router.route('/:id').patch(updateTask);
+
 router.route('/:id').delete(deleteTask);
 
 export default router;
